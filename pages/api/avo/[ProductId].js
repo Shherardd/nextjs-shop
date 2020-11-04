a4c14eb9ccd025820c33d427eaeee27fc885e5cd
Gerardo López <gerardwjones@gmail.com>
2020-11-04 04:14:33 -0600

    API in nextjs - Data and db methods - product by id endpoint

---
+++ pages/api/avo/[ProductId].js
@@ -0,0 +1,21 @@
+import { IncomingMessage, ServerResponse } from 'http'
+import { NextApiRequest, NextApiResponse } from 'next'
+import DB from '../../../database/db'
+
+const ProductId = async (req = NextApiRequest, res = NextApiResponse) => {
+    const ProductId = req.query.ProductId
+    const db = new DB()
+    const product = await db.getById(ProductId)
+    /*
+
+    res.statusCode = 200
+    res.setHeader('Content-type','Application/json')
+    res.end(JSON.stringify({ product }))
+    
+    */
+    // helper de next ->
+    res.status(200).json({product})
+
+}
+
+export default ProductId
--- pages/api/avo/index.js
+++ pages/api/avo/index.js
@@ -1,10 +1,14 @@
 import { IncomingMessage, ServerResponse } from 'http'
+import DB from '../../../database/db'
 
 const Avo = async (req = IncomingMessage, res = ServerResponse) => {
-        
+    const db = new DB()
+    const allEntries = await db.getAll()
+    const length = allEntries.length
+
     res.statusCode = 200
     res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify({ hola: 'mundo' }))
+    res.end(JSON.stringify({ length, data: allEntries }))
 }
 
 export default Avo
